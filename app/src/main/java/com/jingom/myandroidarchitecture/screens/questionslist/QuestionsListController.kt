09277f7c4a5f0715f338d3c0550b271190359f30
박찬준 <chanjun.park@navercorp.com>
2022-03-17 21:45:24 +0900

    Question List 에 다이얼로그 적용

--- app/src/main/java/com/jingom/myandroidarchitecture/screens/questionslist/QuestionsListController.kt
+++ app/src/main/java/com/jingom/myandroidarchitecture/screens/questionslist/QuestionsListController.kt
@@ -2,30 +2,50 @@ package com.jingom.myandroidarchitecture.screens.questionslist
 
 import com.jingom.myandroidarchitecture.questions.FetchQuestionListUseCase
 import com.jingom.myandroidarchitecture.questions.Question
+import com.jingom.myandroidarchitecture.screens.common.dialogs.DialogsEventBus
+import com.jingom.myandroidarchitecture.screens.common.dialogs.DialogsManager
+import com.jingom.myandroidarchitecture.screens.common.dialogs.promptdialogs.PromptDialogEvent
 import com.jingom.myandroidarchitecture.screens.common.screensnavigator.ScreensNavigator
-import com.jingom.myandroidarchitecture.screens.common.toastshelper.ToastHelper
 import kotlinx.coroutines.*
+import java.io.Serializable
 import javax.inject.Inject
 
 class QuestionsListController @Inject constructor(
 	private val screensNavigator: ScreensNavigator,
-	private val toastHelper: ToastHelper,
+	private var dialogsManager: DialogsManager,
 	private val fetchQuestionListUseCase: FetchQuestionListUseCase,
-) : QuestionsListViewMvc.Listener {
+	private val dialogEventBus: DialogsEventBus
+) : QuestionsListViewMvc.Listener, DialogsEventBus.Listener {
+
+	companion object {
+		const val SAVED_STATE_SCREEN_STATE = "SAVED_STATE_SCREEN_STATE"
+		private const val DIALOG_ID_NETWORK_ERROR = "DIALOG_ID_NETWORK_ERROR"
+	}
 
 	private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 	private lateinit var questionsListViewMvc: QuestionsListViewMvc
+
+	private var screenState = ScreenState.IDLE
+
+	enum class ScreenState {
+		IDLE, QUESTIONS_LIST_SHOWN, NETWORK_ERROR
+	}
 
 	fun bindView(questionsListViewMvc: QuestionsListViewMvc) {
 		this.questionsListViewMvc = questionsListViewMvc
 	}
 
 	fun onStart() {
+		dialogEventBus.registerListener(this)
 		questionsListViewMvc.registerListener(this)
-		fetchQuestions()
+
+		if (screenState != ScreenState.NETWORK_ERROR) {
+			fetchQuestions()
+		}
 	}
 
 	fun onStop() {
+		dialogEventBus.unregisterListener(this)
 		questionsListViewMvc.unregisterListener(this)
 		coroutineScope.coroutineContext.cancelChildren()
 	}
@@ -38,9 +58,11 @@ class QuestionsListController @Inject constructor(
 			try {
 				when (val result = fetchQuestionListUseCase.fetchLastActiveQuestions()) {
 					is FetchQuestionListUseCase.Result.Success -> {
+						screenState = ScreenState.QUESTIONS_LIST_SHOWN
 						bindQuestions(result.questions)
 					}
 					is FetchQuestionListUseCase.Result.Failure -> {
+						screenState = ScreenState.NETWORK_ERROR
 						onFetchQuestionsFailed()
 					}
 				}
@@ -55,7 +77,7 @@ class QuestionsListController @Inject constructor(
 	}
 
 	private fun onFetchQuestionsFailed() {
-		toastHelper.showFetchError()
+		dialogsManager.showUseCaseErrorDialog(DIALOG_ID_NETWORK_ERROR)
 	}
 
 	override fun onQuestionClicked(question: Question?) {
@@ -63,4 +85,30 @@ class QuestionsListController @Inject constructor(
 			screensNavigator.toQuestionDetails(question.id)
 		}
 	}
+
+	override fun onDialogEvent(event: Any) {
+		(event as? PromptDialogEvent)?.let {
+			when (it.clickedButton) {
+				PromptDialogEvent.Button.POSITIVE -> {
+					screenState = ScreenState.IDLE
+					fetchQuestions()
+				}
+				PromptDialogEvent.Button.NEGATIVE -> {
+					screenState = ScreenState.IDLE
+				}
+			}
+		}
+	}
+
+	fun restoreSavedState(savedState: SavedState) {
+		this.screenState = savedState.screenState
+	}
+
+	fun getSavedState(): SavedState {
+		return SavedState(screenState)
+	}
+
+	data class SavedState(
+		val screenState: ScreenState
+	): Serializable
 }
--- app/src/main/java/com/jingom/myandroidarchitecture/screens/questionslist/QuestionsListFragment.kt
+++ app/src/main/java/com/jingom/myandroidarchitecture/screens/questionslist/QuestionsListFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.jingom.myandroidarchitecture.screens.common.ViewMvcFactory
-import com.jingom.myandroidarchitecture.screens.common.controllers.BackPressedListener
+import com.jingom.myandroidarchitecture.screens.common.dialogs.DialogsEventBus
+import com.jingom.myandroidarchitecture.screens.common.dialogs.promptdialogs.PromptDialogEvent
+import com.jingom.myandroidarchitecture.screens.questiondetails.QuestionDetailsFragment
+import com.jingom.myandroidarchitecture.screens.questionslist.QuestionsListController.Companion.SAVED_STATE_SCREEN_STATE
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -22,6 +25,12 @@ class QuestionsListFragment: Fragment() {
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 		val questionsListViewMvc = viewMvcFactory.getQuestionListViewMvc(container)
+
+		savedInstanceState?.let {
+			val savedState = it.getSerializable(SAVED_STATE_SCREEN_STATE) as QuestionsListController.SavedState
+			questionsListController.restoreSavedState(savedState)
+		}
+
 		questionsListController.bindView(questionsListViewMvc)
 
 		return questionsListViewMvc.rootView
@@ -35,5 +44,12 @@ class QuestionsListFragment: Fragment() {
 	override fun onStop() {
 		super.onStop()
 		questionsListController.onStop()
+	}
+
+	override fun onSaveInstanceState(outState: Bundle) {
+		super.onSaveInstanceState(outState)
+
+		val savedState = questionsListController.getSavedState()
+		outState.putSerializable(SAVED_STATE_SCREEN_STATE, savedState)
 	}
 }
